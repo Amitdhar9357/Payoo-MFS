8ebc7bfce27ef0da952bdd645dd85c347bb4f181
unknown <amitdhar8778@gmail.com>
2024-09-28 15:42:08 +0600

    added parrially the money event handler

---
+++ js/home.js
@@ -0,0 +1,18 @@
+// console.log('clicked added')
+
+// add money to the account
+
+// step-1: add an event handler to the add money button inside the from
+
+document.getElementById('btn-add-money')
+  .addEventListener('click', function(event){
+    event.preventDefault();
+
+    // step-2: get money to be added to the account
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput)
+    // get the pin number 
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+  })
--- js/login.js
+++ js/login.js
@@ -1,21 +1,41 @@
 // document.getElementById('id').addEventListener('click', fun)
 // step-1 set even handler
-document.getElementById('btn-login')
-      .addEventListener('click', function(event){
-        // set-2: prevent default behavior (prevent reloading browser)
-        event.preventDefault();//<---vajal to beginners
-        console.log('login button clicked')
+// document.getElementById("btn-login")
+//   .addEventListener("click", function (event) {
+//     // set-2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault(); //<---vajal to beginners
+//     console.log("login button clicked");
 
-        //step-3 get the phon number
-        const phonNumber = document.getElementById('phon-number').value;
-        const pinNumber = document.getElementById('pin-number').value;
-        // console.log(phonNumber, pinNumber);
-        
-        //bad way to validate
-        if(phonNumber === '5' && pinNumber === '1234'){
-          console.log('you are logged in');
-        } 
-        else{
-          alert('Wrong phon number or pin');
-        }
-      })
+//     //step-3 get the phon number
+//     const phonNumber = document.getElementById("phon-number").value;
+//     const pinNumber = document.getElementById("pin-number").value;
+//     // console.log(phonNumber, pinNumber);
+
+//     //bad way to validate
+//     if (phonNumber === "5" && pinNumber === "1234") {
+//       console.log("you are logged in");
+//       window.location.href = "/home.html";
+//     } else {
+//       alert("Wrong phon number or pin");
+//     }
+//   });
+
+
+
+
+document.getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); //<---vajal to beginners
+    console.log("login button clicked");
+    const phonNumber = document.getElementById("phon-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    // console.log(phonNumber, pinNumber);
+
+    //bad way to validate
+    if (phonNumber === "5" && pinNumber === "1234") {
+      console.log("you are logged in");
+      window.location.href = '/home.html';
+    } else {
+      alert("Wrong phon number or pin");
+    }
+  });
