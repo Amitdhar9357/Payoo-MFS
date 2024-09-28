01283c0bd94998510c1fb5e079b973f3da58863c
unknown <amitdhar8778@gmail.com>
2024-09-28 12:17:25 +0600

    payoo MFS login section and home page link

---
+++ js/login.js
@@ -0,0 +1,21 @@
+// document.getElementById('id').addEventListener('click', fun)
+// step-1 set even handler
+document.getElementById('btn-login')
+      .addEventListener('click', function(event){
+        // set-2: prevent default behavior (prevent reloading browser)
+        event.preventDefault();//<---vajal to beginners
+        console.log('login button clicked')
+
+        //step-3 get the phon number
+        const phonNumber = document.getElementById('phon-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        // console.log(phonNumber, pinNumber);
+        
+        //bad way to validate
+        if(phonNumber === '5' && pinNumber === '1234'){
+          console.log('you are logged in');
+        } 
+        else{
+          alert('Wrong phon number or pin');
+        }
+      })
